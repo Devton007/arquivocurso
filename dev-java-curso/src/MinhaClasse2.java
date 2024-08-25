c200cf542c9aafa1ecee73d26db43de7ab80c3fa
Devton007 <evertondaniloff@gmail.com>
2024-08-25 16:35:06 -0300

    commit 2

---
+++ dev-java-curso/src/MinhaClasse2.java
@@ -0,0 +1,21 @@
+public class MinhaClasse2 {
+
+    public static void main(String[] args) {
+
+        String primeiroNome = "Ton";
+        String segundoNome = "Ferreira";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+      
+      }
+
+      public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do metodoo  " + primeiroNome.concat("  ").concat(segundoNome);
+
+      }
+      
+     
+
+
+}
