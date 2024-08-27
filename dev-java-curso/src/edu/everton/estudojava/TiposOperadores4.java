9af0659133f62965813190d27d96d9f34814cb03
Devton007 <evertondaniloff@gmail.com>
2024-08-26 22:17:46 -0300

    commitfinal 26/08

---
+++ dev-java-curso/src/edu/everton/estudojava/TiposOperadores4.java
@@ -0,0 +1,27 @@
+package edu.everton.estudojava;
+
+public class TiposOperadores4 {
+
+    public static void main(String[] args) {
+        
+
+    boolean condicao1 = true;
+    boolean condicao2 = true;
+
+    if (condicao1 && condicao2){    
+        
+        System.out.println("ambas as condições são verdadeiras");
+
+    }
+
+        if (condicao1 || condicao2){    
+        
+            System.out.println("uhuuuu");
+
+
+    
+    
+    }
+    
+}
+}
