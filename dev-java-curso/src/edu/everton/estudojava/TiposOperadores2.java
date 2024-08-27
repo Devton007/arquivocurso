5b41e6cb29a8a03d9d95e81490d006feac0281ed
Devton007 <evertondaniloff@gmail.com>
2024-08-26 21:29:06 -0300

    commit1 -26/08

--- dev-java-curso/src/edu/everton/estudojava/TiposOperadores2.java
+++ dev-java-curso/src/edu/everton/estudojava/TiposOperadores2.java
@@ -1,6 +1,32 @@
 package edu.everton.estudojava;
 public class TiposOperadores2 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+
+        // classe Operadores.java
+        int a, b;
+
+          a = 5;
+          b = 5;
+
+        String resultado = "";
+        if (a==b)
+        resultado = "verdadeiro";
+        else
+        resultado = "falso";
+         System.out.println(resultado);
+
+         //agora usando condição ternária
+         int x, y;
+
+         x = 5;
+         y = 6;
+
+       String resultados = a==b ? "verdade" : "mentira";
+
+       System.out.println(resultados);
+
+        
     }
+      
 }
