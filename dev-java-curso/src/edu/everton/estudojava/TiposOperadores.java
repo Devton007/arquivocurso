461e33524ea093eb093746d5fa026f41bab6b475
Devton007 <evertondaniloff@gmail.com>
2024-08-26 21:14:53 -0300

    comitt

--- dev-java-curso/src/edu/everton/estudojava/TiposOperadores.java
+++ dev-java-curso/src/edu/everton/estudojava/TiposOperadores.java
@@ -31,6 +31,25 @@ public class TiposOperadores {
         boolean casado = true;
         System.out.println(! casado);
 
+        //ternário
+        int a, b;
+
+         a = 5;
+         b = 6;
+
+         /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+         if(a==b)
+          resultado = "verdadeiro";
+          else
+          resultado = "falso";
+          */
+
+        //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+         String resultado = (a==b) ? "verdadeiro" : "false";
+
+         System.out.println(valor);
+
+
     }
     
 }
